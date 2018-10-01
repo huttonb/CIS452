e4100c3fb681ef0c7ab6748eadff1f695a209305
Bryce Hutton <huttonb@eos15.cis.gvsu.edu>
2018-10-01 18:06:20 -0400

    fixed read/write addresses. Completely removed dup.

--- project1/test.c
+++ project1/test.c
@@ -10,29 +10,19 @@
 
 int main(){
 	int fd[2];
-	int fd2[2];
-	int fd3[2];
-	int fd4[2];
+	pid_t pid;
 	char* str = "test";
 	char str1[6];
 
 	pipe(fd);
-	pipe(fd2);
-	pipe(fd3);
-	pipe(fd4);
 
-	dup2(fd[READ], fd2[READ]);
-	dup2(fd2[READ], fd3[READ]);
-	dup2(fd3[READ], fd4[READ]);
+	if ((pid = fork()) != 0){
 	write(fd[WRITE], (const void *) str, 6);
-	write(fd[WRITE], (const void *) str, 6);
-//	write(fd[WRITE], (const void *) str, 6);
-	read(fd2[READ], (void*) str1, (size_t) 5);
+	}
+	else{
+	read(fd[READ], (void*) str1, (size_t) 5);
 	printf("\n1:%s", str1);
-	read(fd3[READ], (void*) str1, (size_t) 5);
-	printf("\n2:%s", str1);
-	read(fd4[READ], (void*) str1, (size_t) 5);
-	printf("\n3:%s", str1);
+	}
 	return 0;
 	
 
